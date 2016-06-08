27b46ec22bf96367232317b0a2a0023c4f5d3fb2
zhaoyanjun <362299465@qq.com>
2016-06-08 12:56:47 +0800

    first commit

---
+++ app/src/main/java/app/zuil/com/glidedemo/MainActivity.java
@@ -0,0 +1,13 @@
+package app.zuil.com.glidedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
