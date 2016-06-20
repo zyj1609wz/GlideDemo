d44b4beb9c77983a1d27e7ea9ed70016f43fc673
zhaoyanjun <362299465@qq.com>
2016-06-20 17:38:27 +0800

    本地视频的支持

--- app/src/main/java/app/zuil/com/glidedemo/MainActivity.java
+++ app/src/main/java/app/zuil/com/glidedemo/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.priorityActivity).setOnClickListener(this);
         findViewById(R.id.thumbnailActivity).setOnClickListener(this);
         findViewById(R.id.gifActivity).setOnClickListener(this);
+        findViewById(R.id.videoActivity).setOnClickListener(this);
     }
 
     @Override
@@ -36,6 +37,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.gifActivity :
                 startActivity( new Intent( MainActivity.this , GifActivity.class));
+                break;
+            case R.id.videoActivity :
+                startActivity( new Intent( MainActivity.this , VideoActivity.class));
                 break;
         }
     }
---
+++ app/src/main/java/app/zuil/com/glidedemo/VideoActivity.java
@@ -0,0 +1,27 @@
+package app.zuil.com.glidedemo;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * 只能加载本地视频，只能加载一个图片，不能播放
+ * 网络视频无法加载
+ */
+
+public class VideoActivity extends AppCompatActivity {
+    private ImageView imageView ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        imageView = (ImageView) findViewById( R.id.image_video );
+
+        String files = Environment.getExternalStorageDirectory().getAbsolutePath() + "/yueyu.mkv" ;
+        Glide.with( this ).load( files ).into( imageView ) ;
+    }
+}
